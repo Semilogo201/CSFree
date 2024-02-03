47d2ca0f3108e5eb2690b5f7215169ed83f28fc7
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2024-02-03 22:14:34 +0100

    CSFree

---
+++ budget.py
@@ -0,0 +1,26 @@
+balance = 0
+print("welcome to your budget tracker")
+
+while True:
+    print("1. Add income: ")
+    print("2. Add expenses")
+    print("3. current balance")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice (1-4): "))
+
+    if choice == 1:
+        income_amount = float(input("Enter your income"))
+        balance += income_amount
+        print(f"income of $ {income_amount} added. updated balance: ${balance}")
+    elif choice == 2:
+        expense_amount = float(input("Enter your expenses"))
+        balance -= expense_amount
+        print(f"expense of $ {expense_amount} deducted. ")
+    elif choice == 3:
+        print(f"current_balance of $ {balance}")
+    elif choice == 4:
+        print("Exit the budget tracker program")
+        break
+    else:
+        print("Invalid choice, Please enter a number between 1 and 4")
---
+++ multi.py
@@ -0,0 +1,6 @@
+#Acode that takes input from the user and multiplies it
+n = int(input("Enter a number: "))
+
+for i in range(1,13):
+    result = n * i
+    print(f"{n} x {i} = result")
