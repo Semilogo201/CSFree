f3aface525c9693340b06633a2da35715ac61d7d
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2024-02-06 22:41:30 +0100

    coding

---
+++ 100_Days_coding_Challenge/Day1/Day1.py
@@ -0,0 +1,68 @@
+# #Band name generator
+
+print("Welcome to Gokendro Name Generator")
+name = input("What is the name of the city you grew in? ")
+pet_name = input("What's your pet name? ")
+
+print(f"Your band name could be {name} {pet_name}")
+
+#printing hello world to the screen 
+print("Hello world!")
+
+#syntax highlighting helps you know what is gone wrong with your code. it has to do with the color your write up shows
+#Day1.1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#string duplication
+#The input function(parenthesis then the prompt for the user)
+input("What is your name?")
+# #nesting an input function in a print function
+print("Hello" + input("What is your name?"))
+print("Hello" + " " + input("What is your name?"))
+
+#Usage of input
+name1 = input("what is your name? ")
+print(name1) 
+name2 = input()
+print(name2)
+
+# #Input usage of integer
+num1 = int(input())
+num2 = int(input())
+num3 = int(num1 * num2)
+
+# print(num3) #we were given num1 and num2 to be equal to 2 and 3 respectively
+
+#To determine the length of a string
+name = input()
+print(len(name))
+input() #1.
+len(input()) #2.
+print(len(input()))
+
+#python variables
+names = input("What is your name?") #we are assigning a variable(name) to the string
+print(len(names))
+
+namei = "Jack"
+print(namei)
+
+namei = "Milo"
+print(namei)
+
+print(len(input("what is your name?"))) #this can be rep with variables
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+#swapping contents of a variable
+a = input()
+b = input()
+#to swap(we assign the value of a to another variable c)
+c = a #input c is = to assembly
+a = b #a = b is basic
+b = c
+#Assembly basic will now become Basic Assembly
+#Naming Variables(make your code readable)
